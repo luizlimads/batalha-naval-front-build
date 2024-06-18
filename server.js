941aaf1ff8e3fcc8e0b39a7bf4e681f49fbcdec8
luizlimads <luizhenrique.limads@gmail.com>
2024-06-18 11:11:57 -0300

    build: acept hhtp node

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+const http = require('http');
 const express = require('express');
 const path = require('path');
 
 const app = express();
+
+// Middleware para redirecionar HTTPS para HTTP
+app.use((req, res, next) => {
+  if (req.secure) {
+    // Redirecionar para HTTP
+    return res.redirect(`http://${req.headers.host}${req.url}`);
+  }
+  next();
+});
 
 // Servir os arquivos estáticos da pasta dist
 const appPath = path.join(__dirname, 'dist');
@@ -9,11 +19,11 @@ app.use(express.static(appPath));
 
 // Redirecionar todas as requisições para o index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(appPath, 'index.html'));
+  res.sendFile(path.join(appPath, 'index.html'));
 });
 
-// Definir a porta e iniciar o servidor
+// Definir a porta e iniciar o servidor HTTP
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+http.createServer(app).listen(PORT, () => {
+  console.log(`Servidor HTTP rodando na porta ${PORT}`);
 });
